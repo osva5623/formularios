8862a9b267cfd47a470ab7ed3939c8e8d68c6e62
osva5623 <67302639+osva5623@users.noreply.github.com>
2025-08-13 10:11:41 -0600

    Update formularios.blade.php

--- resources/views/formularios.blade.php
+++ resources/views/formularios.blade.php
@@ -85,7 +85,7 @@ document.getElementById("myForm").addEventListener("submit", async function(e) {
     const data = Object.fromEntries(formData.entries());
 
     try {
-        const response = await fetch("https://76218134d259.ngrok-free.app/submit", {
+        const response = await fetch("https://a3484f7d6b87.ngrok-free.app/submit", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
